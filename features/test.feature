e62c60140fca730652e9e933f855d5df8c68890f
Sergey Shakur <sergey.shakur@gmail.com>
2023-07-03 16:00:35 +0300

    added step for pausing a step

--- features/test.feature
+++ features/test.feature
@@ -2,5 +2,6 @@ Feature: My test
 
   Scenario: counting items
     Given I start with 2 items
+    And I pause for 1 second
     When I add 3 items
     Then I have 5 items
--- src/steps.rs
+++ src/steps.rs
@@ -1,3 +1,5 @@
+use std::time::Duration;
+
 use cucumber::{given, then, when};
 
 use crate::world::MyWorld;
@@ -15,4 +17,10 @@ async fn add_count(world: &mut MyWorld, count: i32) {
 #[then(expr = "I have {int} items")]
 async fn check_result(world: &mut MyWorld, count: i32) {
     assert_eq!(world.count, count);
+}
+
+#[when(regex = r#"I pause for (\d+) seconds?"#)]
+#[given(regex = r#"I pause for (\d+) seconds?"#)]
+async fn add_delay(_: &mut MyWorld, delay: u64) {
+    tokio::time::sleep(Duration::from_secs(delay)).await;
 }
